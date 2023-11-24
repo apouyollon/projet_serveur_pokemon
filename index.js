be256325e3f739e1a5f9b687fa66176db7b99b42
Antoine Pouyollon <antoine.pouyollon@gmail.com>
2023-11-24 04:41:23 +0100

    show_pokemon_ok

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let g_list_pokemon = [];
 let g_list_gen = [];
 let g_list_type = [];
 
-
 // requête Fetch
 async function get_pokemon(callback_1,callback_2) {
     const response = await fetch('https://tyradex.vercel.app/api/v1/pokemon', {
@@ -16,6 +15,9 @@ async function get_pokemon(callback_1,callback_2) {
     g_list_pokemon = new_result;
     // on stocke la liste des pokémons dans le tableau g_list_pokemon
     console.log(g_list_pokemon);
+    // génère la fiche d'un pokémon tiré aléatoirement entre 1 et la longueur du tableau
+    // à chaque rechargement de la page ou à chaque arrivée sur la page d'accueil
+    show_pokemons_reduct(Math.floor(Math.random() * g_list_pokemon.length)+1);
     callback_1(new_result);
     // on envoie la réponse du serveur à une nouvelle fonction
     // qui ne récupérera qu'une partie des valeurs du tableau
@@ -45,13 +47,15 @@ function get_list_gen(p_tab_pokemon) {
 function get_list_type(p_tab_pokemon) {
     var interim_type_tab = [];
     for (let i=0; i<p_tab_pokemon.length; i++) {
-        for (let j=0; j<p_tab_pokemon[i].types.length; i++) {
-            interim_type_tab.push(p_tab_pokemon[i].name);
+        // création d'un deuxième tableau pour parcourir plusieurs éléments
+        var type_array = p_tab_pokemon[i].types;
+        for (let j=0; j<type_array.length; j++) {
+            interim_type_tab.push(type_array[j].name);
         }
     }
     g_list_type = remove_duplicates_in_tab(interim_type_tab);
     console.log(g_list_type);
-    show_list_gen();
+    show_list_type();
 }
 
 function remove_duplicates_in_tab(p_tab){
@@ -95,7 +99,6 @@ function show_list_gen() {
 
 function show_pokemons_in_list_gen(p_generation) {
     //affiche les noms des pokemons d'une génération précise
-    console.log(p_generation);
     for (let i=0; i<g_list_gen.length; i++){
         // ne garder affichée qu'une génération à la fois     
         if (g_list_gen[i] == p_generation) {
@@ -110,13 +113,24 @@ function show_pokemons_in_list_gen(p_generation) {
 function show_list_type() {
     // affiche une liste contenant toutes les générations de pokemon
     for (let i=0; i<g_list_type.length; i++){
-        let list_type_elem = `<li onclick='show_pokemons_in_list_gen(`+g_list_type[i]+`)' 
+        let list_type_elem = `<li onclick='show_pokemons_in_list_type("`+ g_list_type[i] +`")' 
         class='list_box'>Type ` + g_list_type[i] + `<ul id='type_` + g_list_type[i]+ `'></ul></li>`;
-        $('#list_gen').append(list_type_elem);
+        $('#list_type').append(list_type_elem);
     }
-    for (let i=0; i<=g_list_type.length; i++){
-        var list_type_pokemons = g_list_pokemon.filter(e=>e.types == g_list_type[i]);
-        console.log(list_type_pokemons);
+    // récupérer chaque type
+    for (let i=0; i<g_list_type.length; i++){
+        var list_type_pokemons = [];
+        // pour chaque type, comparer chaque pokemon
+        for (let k=0; k<g_list_pokemon.length; k++){
+            // pour chaque pokemon, récuérer chaque type
+            var pokemon_type_array = g_list_pokemon[k].types;
+            // ajouter au tableau list_type_pokemons ceux correspondant à g_list_type[i]
+            var interim_list_type = pokemon_type_array.filter(e=>e.name == g_list_type[i]);
+            if (interim_list_type.length >= 1) {
+                list_type_pokemons.push(g_list_pokemon[k]);
+            }
+        }
+        // console.log(list_type_pokemons);     
         for (let j=0; j<list_type_pokemons.length; j++){
             let pokemon_type_elem = `<li onclick='show_pokemons_details(` + list_type_pokemons[j].pokedexId + `)' 
             class='pokemon_name'>` + list_type_pokemons[j].name.fr + `</li>`;
@@ -126,9 +140,8 @@ function show_list_type() {
 }
 
 function show_pokemons_in_list_type(p_type) {
-    console.log(p_type);
     for (let i=0; i<g_list_type.length; i++){  
-        if (g_list_gen[i] == p_generation) {
+        if (g_list_type[i] == p_type) {
             $('#type_' + g_list_type[i]).slideToggle(750);
         } 
         else {
@@ -146,15 +159,130 @@ function show_pokemons_in_list_type(p_type) {
 
 // fonctions show_pokemons_ : affichent les fiches d'un pokémon dans une div
 function show_pokemons_details(p_id) {
-    return;
+    var pokemon_to_show = g_list_pokemon.filter((e) => e.pokedexId==p_id)[0];
+    console.log(pokemon_to_show);
+
+    let pokemon_detail = template_reduct_pokemon(pokemon_to_show);
+
+    $('#pokemon_details').html(pokemon_detail);
+    
+    // n'afficher les évolutions que si elles existent
+    if (pokemon_to_show.evolution.pre != null){
+        if (pokemon_to_show.evolution.pre.length == 2) {
+            let evolution_precedente = `<div class='evolution'>
+            <h3>Evolutions précédentes : </h3> 
+            <p>`+ pokemon_to_show.evolution.pre[0].name +`</p>
+            <img src='`+ g_list_pokemon.filter((e) => e.pokedexId == (pokemon_to_show.evolution.pre[0].pokedexId))[0].sprites.regular +`'  
+            onclick='show_pokemons_details(`+ pokemon_to_show.evolution.pre[0].pokedexId +`)' class='images_evolution'/>
+            <p>`+ pokemon_to_show.evolution.pre[1].name +`</p>
+            <img src='`+ g_list_pokemon.filter((e) => e.pokedexId == (pokemon_to_show.evolution.pre[1].pokedexId))[0].sprites.regular +`'  
+            onclick='show_pokemons_details(`+ pokemon_to_show.evolution.pre[1].pokedexId +`)' class='images_evolution'/>
+            </div>`;
+            // image cliquable rechargeant la fonction pour afficher l'évolution
+            $('#pokemon_details').append(evolution_precedente);  
+        }
+        else {
+            let evolution_precedente = `<div class='evolution'>
+            <h3>Evolution précédente : </h3> 
+            <p>`+ pokemon_to_show.evolution.pre[0].name +`</p>
+            <img src='`+ g_list_pokemon.filter((e) => e.pokedexId == (pokemon_to_show.evolution.pre[0].pokedexId))[0].sprites.regular +`'  
+            onclick='show_pokemons_details(`+ pokemon_to_show.evolution.pre[0].pokedexId +`)' class='images_evolution'/>
+            </div>`;
+            // image cliquable rechargeant la fonction pour afficher l'évolution
+            $('#pokemon_details').append(evolution_precedente);
+        }   
+    }
+
+    if (pokemon_to_show.evolution.next != null){
+        if (pokemon_to_show.evolution.next.length == 2) {
+            let evolution_suivante = `<div class='evolution'>
+            <h3>Evolutions suivantes : </h3>
+            <p>`+ pokemon_to_show.evolution.next[0].name +`</p> 
+            <img src='`+ g_list_pokemon.filter((e) => e.pokedexId == (pokemon_to_show.evolution.next[0].pokedexId))[0].sprites.regular +`'
+            onclick='show_pokemons_details(`+ pokemon_to_show.evolution.next[0].pokedexId +`)' class='images_evolution'/> 
+            <p>`+ pokemon_to_show.evolution.next[1].name +`</p>
+            <img src='`+ g_list_pokemon.filter((e) => e.pokedexId == (pokemon_to_show.evolution.next[1].pokedexId))[0].sprites.regular +`'
+            onclick='show_pokemons_details(`+ pokemon_to_show.evolution.next[1].pokedexId +`)' class='images_evolution'/>
+            </div>`;
+            $('#pokemon_details').append(evolution_suivante);
+        }
+        else {
+            let evolution_suivante = `<div class='evolution suivante'>
+            <h3>Evolution suivante : </h3>
+            <p>`+ pokemon_to_show.evolution.next[0].name +`</p>
+            <img src='`+ g_list_pokemon.filter((e) => e.pokedexId == (pokemon_to_show.evolution.next[0].pokedexId))[0].sprites.regular +`'
+            onclick='show_pokemons_details(`+ pokemon_to_show.evolution.next[0].pokedexId +`)' class='images_evolution'/>
+            </div>`;
+            $('#pokemon_details').append(evolution_suivante);  
+        }
+    }
 }
 
 function show_pokemons_reduct(p_id) {
-    return;
+    var pokemon_to_show = g_list_pokemon.filter((e) => e.pokedexId==p_id)[0];
+    console.log(pokemon_to_show);
+    let pokemon_reduct = template_reduct_pokemon(pokemon_to_show);
+    $('#pokemon_reduct').html(pokemon_reduct);
+}
+
+function template_reduct_pokemon(pokemon_to_show) {
+    // création de 2 versions en fonction du nombre de types des pokémons
+    if (pokemon_to_show.types.length == 2) {
+        let pokemon_template = `<div class='pokemon_description'>
+        <img src='`+ pokemon_to_show.sprites.regular +`' class='image_pokemon'/> 
+        <h3>Nom : `+ pokemon_to_show.name.fr +`</h3>
+        <p>Numéro : `+ pokemon_to_show.pokedexId +`</p>
+        <p>Types : `+ pokemon_to_show.types[0].name +` / `+ pokemon_to_show.types[1].name +`</p>
+        <p>Génération : `+ pokemon_to_show.generation +`</p>
+        </div>`;
+        return pokemon_template;
+    }
+    else {
+        let pokemon_template = `<div class='pokemon_description'>
+        <img src='`+ pokemon_to_show.sprites.regular +`' class='image_pokemon'/> 
+        <h3>Nom : `+ pokemon_to_show.name.fr +`</h3>
+        <p>Numéro : `+ pokemon_to_show.pokedexId +`</p>
+        <p>Type : `+ pokemon_to_show.types[0].name +`</p>
+        <p>Génération : `+ pokemon_to_show.generation +`</p>
+        </div>`;
+        return pokemon_template;
+    }
 }
 
 
 
 
 
+// fonctions de navigation dans le menu
+function display_sections(num) {
+    if (num == 1) {
+        $('#accueil').show();
+        $('#generation').hide();
+        $('#type').hide();
+        $('#pokemon_details').hide();
+        show_pokemons_reduct(Math.floor(Math.random() * g_list_pokemon.length)+1);
+    }
+    else if (num == 2) {
+        $('#accueil').hide();
+        $('#generation').show();
+        $('#type').hide();
+        $('#pokemon_details').show().html(''); 
+    }
+    else {
+        $('#accueil').hide();
+        $('#generation').hide();
+        $('#type').show();
+        $('#pokemon_details').show().html('');  
+    }
+
+}
+
+
+
+
+
+
+// init functions : lancées au démarrage après la requête fetch
 get_pokemon(get_list_gen, get_list_type);
+
+display_sections(1);
